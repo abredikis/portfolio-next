a6a02fddc5af69dcb191c27c422635efc5128dd8
albert <albertsbredikis@gmail.com>
2024-05-29 18:53:30 +0200

    feat(tailwind): add custom theme style config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
-import fluid, { extract } from "fluid-tailwind";
-
+import fluid, { extract, fontSize } from "fluid-tailwind";
+import { FluidThemeConfig } from "fluid-tailwind";
 const config: Config = {
   content: {
     files: [
@@ -11,12 +11,60 @@ const config: Config = {
     extract,
   },
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    fontSize,
+    screens: {
+      xs: "20rem", // 320px
+      sm: "30rem", // 480px
+      md: "48rem", // 768px
+      lg: "62rem", // 992px
+      xl: "75rem", // 1200px
+      xxl: "90rem", // 1440px
+      fhd: "100rem", // 1600px
+      qhd: "120rem", // 1920px
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000000",
+      white: "#FFFFFF",
+      primary: {
+        50: "#F5F8FF",
+        100: "#DBE6FF",
+        200: "#A8C2FF",
+        300: "#709BFF",
+        400: "#3D77FF",
+        500: "#0A54FF",
+        600: "#0040D6",
+        700: "#002F9E",
+        800: "#00206B",
+        900: "#000F33",
+        950: "#000819",
       },
+      accent: {
+        50: "#FFF5F8",
+        100: "#FFDBE6",
+        200: "#FFA8C2",
+        300: "#FF709B",
+        400: "#FF3D77",
+        500: "#FF0A54",
+        600: "#D60040",
+        700: "#9E002F",
+        800: "#6B0020",
+        900: "#33000F",
+        950: "#190008",
+      },
+    },
+    fontFamily: {
+      sans: ["DM Sans", "sans-serif"],
+    },
+    fluid: (({ theme }) => ({
+      defaultScreens: [theme("screens.sm"), theme("screens.xxl")],
+    })) satisfies FluidThemeConfig,
+    transitionTimingFunction: {
+      DEFAULT: "cubic-bezier(0.79,0.14,0.15,0.86);",
+    },
+    transitionDuration: {
+      DEFAULT: "250ms",
     },
   },
   plugins: [fluid],
