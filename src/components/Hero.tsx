efddc4652a831c667e9893bbd9e7134c31fad469
albert <albertsbredikis@gmail.com>
2024-06-04 16:15:59 +0200

    feat(homepage): layout improvements, homepage content

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,20 +7,20 @@ type HeroProps = React.ComponentPropsWithoutRef<'section'>;
 export default function Hero({ ...props }: HeroProps) {
   return (
     <section className='relative flex min-h-[95dvh] flex-col justify-center'>
-      <div className='mx-auto w-full max-w-[90rem] ~px-6/24'>
-        <div className='flex flex-wrap items-center justify-center ~py-16/24 lg:flex-nowrap'>
-          <div className='relative mb-8 w-full max-w-[405px] overflow-hidden rounded-xl lg:mb-0'>
+      <div className='mx-auto w-full max-w-[90rem] ~px-6/24 ~py-16/24'>
+        <div className='flex flex-wrap items-center justify-center md:~sm/xxl:~-mx-3/8 lg:flex-nowrap'>
+          <div className='relative mb-8 w-full max-w-[469px] md:~sm/xxl:~px-3/8 lg:mb-0'>
             <Image
               src={authorImage}
               priority={true}
               quality={100}
               alt='Portrait of Alberts Bredikis'
               sizes='405px'
-              className='relative z-0 aspect-[1/1.22] w-full object-cover'
+              className='relative z-0 aspect-[1/1.22] w-full rounded-xl object-cover'
             />
             <div className='pointer-events-none absolute inset-0 z-10 bg-primary-500 bg-opacity-10 mix-blend-lighten'></div>
           </div>
-          <div className='basis-full pl-0 md:basis-auto md:~sm/xxl:~px-6/16'>
+          <div className='basis-full pl-0 md:basis-auto md:~sm/xxl:~px-3/8'>
             <h1 className='mb-4 text-center font-medium !leading-[1.1] tracking-tight ~/md:~text-4xl/5xl md:~text-5xl/6xl lg:text-left'>
               Hey, <br className='hidden xl:inline' /> I'm Albert
             </h1>
@@ -30,11 +30,9 @@ export default function Hero({ ...props }: HeroProps) {
               experience and improve SEO performance.
             </p>
             <div className='flex flex-wrap ~gap-3/4'>
-              <Link href='#projects' passHref={true} legacyBehavior>
-                <Button className='min-w-fit flex-grow basis-0 xl:flex-grow-0'>
-                  Get in touch
-                </Button>
-              </Link>
+              <Button className='min-w-fit flex-grow basis-0 xl:flex-grow-0'>
+                Get in touch
+              </Button>
               <Button
                 className='min-w-fit flex-grow basis-0 xl:flex-grow-0'
                 variant='secondary'
