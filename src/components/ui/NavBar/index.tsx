012c55f4d8bcb6fe8e16890068ecd8157f508598
albert <albertsbredikis@gmail.com>
2024-06-14 13:56:26 +0200

    refactor(navbar): remove unnecessary index file

--- src/components/ui/NavBar/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as NavBar } from './NavBar';
-export { default as NavButton } from './NavButton';
-export { default as NavLink } from './NavLink';
-export { default as NavItem } from './NavItem';
