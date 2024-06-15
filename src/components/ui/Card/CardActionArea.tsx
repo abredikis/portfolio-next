58b130758b087145e1901aedb66da8fab186ac19
albert <albertsbredikis@gmail.com>
2024-06-15 19:10:40 +0200

    refactor(card): refactor card action area into a polymorphic component with conditional rendering

--- src/components/ui/Card/CardActionArea.tsx
+++ src/components/ui/Card/CardActionArea.tsx
@@ -5,24 +5,48 @@ import fixedForwardRef from '@/utils/fixedForwardRef';
 import mergeRefs from 'merge-refs';
 import Ripple from '@/components/Ripple';
 import { cn } from '@/utils/cn';
+import { Merge } from '@/types/utilities';
 
-type CardActionAreaProps = {
-  showRipple?: boolean;
-} & React.ComponentPropsWithRef<'button'>;
+type CardActionAreaAllowedTags = React.ElementType<any, 'a' | 'button'>;
 
-export default fixedForwardRef(function CardActionArea(
-  { showRipple = true, className, children, ...rest }: CardActionAreaProps,
-  ref: React.ForwardedRef<HTMLButtonElement>
+type CardActionAreaProps<T extends CardActionAreaAllowedTags = 'a'> = Merge<
+  React.ComponentPropsWithRef<T>,
+  {
+    as?: T;
+    isRippleVisible?: boolean;
+  }
+>;
+
+export default fixedForwardRef(function CardActionArea<
+  T extends CardActionAreaAllowedTags = 'a',
+>(
+  {
+    as = 'a' as T,
+    isRippleVisible = true,
+    className,
+    children,
+    ...rest
+  }: CardActionAreaProps<T>,
+  ref: React.ForwardedRef<React.ElementRef<T>>
 ) {
-  const localRef = useRef<HTMLButtonElement>(null);
-  return (
-    <button
+  const Tag: CardActionAreaAllowedTags = as;
+  const localRef = useRef<React.ElementRef<T>>(null);
+
+  return rest?.href || rest?.onClick ? (
+    <Tag
       ref={mergeRefs(ref, localRef)}
       {...rest}
       className={cn('d-block relative w-full overflow-hidden', className)}
     >
       {children}
-      {showRipple && <Ripple targetRef={localRef} variant='secondary' />}
-    </button>
+      {isRippleVisible && (
+        <Ripple
+          targetRef={localRef as React.RefObject<HTMLElement>}
+          variant='secondary'
+        />
+      )}
+    </Tag>
+  ) : (
+    <>{children}</>
   );
 });
