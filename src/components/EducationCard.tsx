fad85f6f98b9004897aef678c05ad2fa2fd0abf2
albert <albertsbredikis@gmail.com>
2024-06-16 18:01:06 +0200

    feat(education-card): implement education card and types

---
+++ src/components/EducationCard.tsx
@@ -0,0 +1,41 @@
+import { Card } from './ui/Card';
+import CardContent from './ui/Card/CardContent';
+import { CardProps } from './ui/Card/Card';
+import { cn } from '@/utils/cn';
+import CardActionArea from './ui/Card/CardActionArea';
+import { Education } from '@/types/education';
+
+type EducationCardProps = {
+  education: Education;
+} & CardProps;
+
+export default function EducationCard({
+  className,
+  children,
+  education,
+  ...rest
+}: EducationCardProps) {
+  return (
+    <Card size='small' {...rest} className={cn('h-auto', className)}>
+      <CardActionArea href={education.url}>
+        <CardContent className='flex h-full flex-col'>
+          <div className=''>
+            <h3 className='mt-0 text-balance text-xl font-semibold text-primary-600'>
+              {education.title}
+            </h3>
+            <p className='mb-0 text-sm'>
+              <span className='text-primary-500'> {education.source}</span>
+              {(education?.dateFrom || education?.dateTo) && (
+                <span className='mx-2 font-extrabold'>·</span>
+              )}
+              {[education?.dateFrom, education?.dateTo]
+                .filter((date) => date)
+                .join(' - ')}
+            </p>
+          </div>
+          <div className='not-prose mt-6'>{education?.content || children}</div>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
