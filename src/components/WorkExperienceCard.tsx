2a0f56bb9858115516f8de5bd6ba5ef123f0f0b5
albert <albertsbredikis@gmail.com>
2024-06-17 15:53:03 +0200

    feat(work-experience-card): add external link icon, typography adjustments, add border-radius

--- src/components/WorkExperienceCard.tsx
+++ src/components/WorkExperienceCard.tsx
@@ -6,6 +6,7 @@ import Tag from './ui/Tag';
 import { cn } from '@/utils/cn';
 import CardActionArea from './ui/Card/CardActionArea';
 import { WorkExperience } from '@/types/workExperience';
+import { fromTheme } from 'tailwind-merge';
 
 type WorkExperienceCardProps = {
   workExperience: WorkExperience;
@@ -24,19 +25,32 @@ export default function WorkExperienceCard({
           <div className='flex flex-wrap items-center gap-x-6 gap-y-4'>
             {workExperience.logoUrl && (
               <Image
-                className='m-0 shrink-0 ~size-14/16 ~max-w-14/16'
+                className='m-0 shrink-0 overflow-hidden rounded-xl ~size-14/16 ~max-w-14/16'
                 src={workExperience.logoUrl}
                 width={64}
                 height={64}
                 alt={`Logo of ${workExperience.title}`}
               />
             )}
-            <div className=''>
-              <h3 className='mt-0 text-balance text-2xl font-semibold text-primary-600'>
+            <div>
+              <h3 className='mt-0 text-balance font-semibold text-primary-600 ~text-xl/2xl'>
                 {workExperience.role}
               </h3>
-              <p className='mb-0 text-sm'>
-                <span className='text-primary-500'>{workExperience.title}</span>
+              <p className='mb-0 flex text-sm'>
+                <span className='flex gap-1 text-primary-500'>
+                  {workExperience.title}{' '}
+                  {workExperience.url && (
+                    <svg
+                      width='14'
+                      height='14'
+                      fill='#0A54FF'
+                      viewBox='0 0 24 24'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path d='M10 6v2H5v11h11v-5h2v6a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h6Zm11-3v8h-2V6.413l-7.793 7.794-1.414-1.414L17.585 5H13V3h8Z'></path>
+                    </svg>
+                  )}
+                </span>
                 <span className='mx-2 font-extrabold'>·</span>
                 {[workExperience?.dateFrom, workExperience?.dateTo]
                   .filter((date) => date)
