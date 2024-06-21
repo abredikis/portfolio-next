c5df7e89634f6186dabc043788015b7cd3b327d6
albert <albertsbredikis@gmail.com>
2024-06-21 21:06:35 +0200

    style(education-card): refactor card typography add hover arrow

--- src/components/EducationCard.tsx
+++ src/components/EducationCard.tsx
@@ -19,21 +19,54 @@ export default function EducationCard({
     <Card size='small' {...rest} className={cn('h-auto', className)}>
       <CardActionArea href={education.url} target='_blank'>
         <CardContent className='flex h-full flex-col'>
-          <div className=''>
-            <h3 className='mt-0 text-balance text-xl font-semibold text-primary-600'>
+          <div>
+            <h3 className='mb-[0.2em] text-balance font-semibold leading-tight text-primary-600 ~text-lg/xl'>
               {education.title}
             </h3>
-            <p className='mb-0 text-sm'>
-              <span className='text-primary-500'> {education.source}</span>
+            <p className='text-sm'>
+              <span className='text-primary-500'>
+                {' '}
+                {education.source}
+                {education.url && (
+                  <span className='relative ml-1 inline-flex size-3 items-center justify-center overflow-hidden'>
+                    <svg
+                      className='absolute inset-0 size-3 -translate-x-3 translate-y-3 transition-transform group-hover:translate-x-0 group-hover:translate-y-0'
+                      fill='currentColor'
+                      stroke='currentColor'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2.2'
+                      viewBox='0 0 24 24'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path d='M5.636 18.364 18.364 5.636m0 0h-9.9m9.9 0v9.9'></path>
+                    </svg>
+                    <svg
+                      className='absolute inset-0 size-3 translate-x-0 translate-y-0 transition-transform group-hover:-translate-y-3 group-hover:translate-x-3'
+                      fill='currentColor'
+                      stroke='currentColor'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2.2'
+                      viewBox='0 0 24 24'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <path d='M5.636 18.364 18.364 5.636m0 0h-9.9m9.9 0v9.9'></path>
+                    </svg>
+                  </span>
+                )}
+              </span>
               {(education?.dateFrom || education?.dateTo) && (
-                <span className='mx-2 font-extrabold'>·</span>
+                <span className='mx-3 font-extrabold'>·</span>
               )}
               {[education?.dateFrom, education?.dateTo]
                 .filter((date) => date)
                 .join(' - ')}
             </p>
           </div>
-          <div className='not-prose mt-6'>{education?.content || children}</div>
+          <div className='mt-5 max-w-[65ch]'>
+            {education?.content || children}
+          </div>
         </CardContent>
       </CardActionArea>
     </Card>
