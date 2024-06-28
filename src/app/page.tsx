718a6e23fcda353cd4538151842cd74d05435c35
albert <albertsbredikis@gmail.com>
2024-06-28 12:59:04 +0200

    feat(homepage): add footer and background accent components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,20 @@ import Hero from '@/components/Hero';
 import ExperienceEducation from '@/components/ExperienceEducation';
 import WithNavBar from '@/components/WithNavBar';
 import ProjectGrid from '@/components/ProjectGrid';
+import WithFooter from '@/components/WithFooter';
+import WithBackgroundAccent from '@/components/WithBackgroundAccent';
 
 export default function Home() {
   return (
+    <>
+      <WithBackgroundAccent />
       <main className='z-10 flex-grow pt-24 leading-relaxed text-primary-900/80'>
+        <WithNavBar />
+        <Hero />
+        <ExperienceEducation />
+        <ProjectGrid />
+        <WithFooter />
+      </main>
+    </>
   );
 }
