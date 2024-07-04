bf2a0bfb82b9b6329d8a269f8ec9f3a98a7130ef
albert <albertsbredikis@gmail.com>
2024-07-04 22:29:55 +0200

    feat(slide-up): implement slide up animation component

---
+++ src/components/ui/Animations/SlideUp.tsx
@@ -0,0 +1,75 @@
+'use client';
+import { forwardRef, useRef } from 'react';
+import Animate, {
+  AnimateAllowedTags,
+  AnimateProps,
+  PolymorphicRef,
+} from './Animate';
+import useTimeline from '../../../hooks/useTimeline';
+import mergeRefs from 'merge-refs';
+
+type SlideUpIntensity = 'weak' | 'normal' | 'strong';
+
+type SlideUpProps<T extends AnimateAllowedTags = 'div'> = {
+  intensity: 'weak' | 'normal' | 'strong';
+  createTimelineOptions?: (
+    target?: gsap.DOMTarget
+  ) => gsap.TimelineVars | undefined;
+} & AnimateProps<T>;
+
+const ANIMATION_INTENSITY: Record<SlideUpIntensity, gsap.TweenVars> = {
+  weak: { translateY: '15%', opacity: 0 },
+  normal: { translateY: '50%', opacity: 0 },
+  strong: { translateY: '100%', opacity: 0 },
+};
+
+export default forwardRef(function SlideUp<
+  T extends AnimateAllowedTags = 'div',
+>(
+  {
+    intensity = 'normal',
+    timeline,
+    createTimelineOptions = (target) => {
+      return {
+        scrollTrigger: {
+          trigger: target,
+          start: 'top bottom-=10%',
+        },
+      };
+    },
+    config,
+    ...rest
+  }: SlideUpProps<T>,
+  ref: PolymorphicRef<T>
+) {
+  const localRef = useRef<HTMLElement>(null);
+
+  const localTimeline = useTimeline({
+    timeline,
+    createTimelineOptions,
+    elementRef: localRef,
+  });
+
+  return (
+    <Animate
+      {...rest}
+      ref={mergeRefs(ref, localRef)}
+      timeline={localTimeline}
+      config={{
+        ...{
+          from: ANIMATION_INTENSITY[intensity],
+          to: {
+            translateY: 0,
+            opacity: 1,
+            ease: 'expo.out',
+            duration: 1.625,
+            stagger: {
+              amount: 0.4,
+            },
+          },
+        },
+        ...config,
+      }}
+    />
+  );
+});
